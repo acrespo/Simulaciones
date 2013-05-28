8c3d51ee474a3bc6a425ef9aeb8f224f194f9a4e
Juan Pablo Civile <elementohb@gmail.com>
2013-05-28 19:22:01 -0300

    Adjust sampling to the last changes

--- sim/sample.py
+++ sim/sample.py
@@ -13,20 +13,30 @@ def project_size():
     else:
         return 2
 
-def project(hours_min, hours_max, hours_mode, price_min, price_max, price_mode, delivery_min, delivery_max, delivery_mode):
+def workforce_sample(min):
+    u = random.uniform()
+    if u < 0.30:
+        return min
+    elif u < 0.8:
+        return min + 1
+    else:
+        return min + 2
+
+def project(hours_min, hours_max, hours_mode, price_min, price_max, price_mode, delivery_min):
     hours = int(random.triangular(hours_min, hours_mode, hours_mode))
     price = int(random.triangular(price_min, price_mode, price_max))
 
-    delivery = ceil(random.triangular(hours / (4 * 40.0 * delivery_min), hours / (4 * 40.0 * delivery_mode), hours / (4 * 40.0 * delivery_max)))
+
+    delivery = ceil(hours / (4 * 40.0 * workforce_sample(delivery_min)))
 
     return (hours, price, delivery)
 
 def small_project():
-    return project(500, 2000, 1400, 242, 308, 286, 2.0, 1.0, 1.2)
+    return project(500, 2000, 1400, 260, 310, 280, 1)
 
 def medium_project():
-    return project(2000, 4500, 3200, 198, 242, 220, 4.5, 2.0, 2.5)
+    return project(2000, 4500, 3200, 200, 290, 240, 3)
 
 def big_project():
-    return project(4500, 8000, 5300, 176, 308, 226, 6.0, 3.0, 4.0)
+    return project(4500, 8000, 5300, 180, 250, 200, 5)
 
